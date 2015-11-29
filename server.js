1543f28ca6f2e2b09bba17d94dac648732d559d2
Rubén Melo <rbmcar2@gmail.com>
2015-11-29 13:43:39 -0500

    serving at 80

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express"),
 	server = require("http").createServer(app),
 	io = require("socket.io").listen(server);
 
-server.listen(3000);
+server.listen(80);
 
 //static server
 app.use(express.static(__dirname));
